e4ed670bccf4ffb207f9eb622497556b71928c52
shinano <hieuntskk@gmail.com>
2024-11-07 23:42:37 +0700

    create app

--- Blind/app/build.gradle.kts
+++ Blind/app/build.gradle.kts
@@ -1,23 +1,20 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.example.blind"
-    compileSdk = 34
+    namespace = "com.hmi.blind"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.blind"
-        minSdk = 24
+        applicationId = "com.hmi.blind"
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -30,40 +27,39 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+
     buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        viewBinding = true
     }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+
+    implementation(libs.tensorflow.tensorflow.lite)
+    implementation(libs.tensorflow.lite.support)
+
+    implementation(libs.tensorflow.lite.gpu.delegate.plugin)
+    implementation(libs.tensorflow.lite.gpu.api)
+    implementation(libs.tensorflow.lite.api)
+    implementation(libs.tensorflow.lite.gpu)
+    implementation(libs.tensorflow.lite.select.tf.ops)
+
+    implementation(libs.tensorflow.lite.metadata)
 }
---
+++ Blind/app/src/main/java/com/hmi/blind/Constants.kt
@@ -0,0 +1,6 @@
+package com.hmi.blind
+
+object Constants {
+    const val MODEL_PATH = "data_training.tflite"
+    val LABELS_PATH: String? = "labels.txt" // provide your labels.txt file if the metadata not present in the model
+}
